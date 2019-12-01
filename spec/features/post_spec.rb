eae4dac734581d6baa0bebc7c32bcb4642d07d7b
mdegraw <michael.degraw@semanticbits.com>
2019-12-01 14:56:36 -0700

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -35,7 +35,10 @@ describe 'form' do
 
   it 'shows an update form that submits content and redirects and prints out params' do
     @post = Post.create(title: "My Post", description: "My post desc")
-
+    puts '\n\n\n\n\n\n@post'
+    puts @post.title
+    puts @post.id
+    puts @post.description
     visit edit_post_path(@post)
 
     fill_in 'post[title]', with: "My edit"
